da1aa137cbf89fa8c690274f22978f24fbf21755
dkiet519-spec <dkiet519@gmail.com>
2026-04-18 11:17:39 +0700

    Hoc Scanner & HelloWorld

---
+++ HelloWorld/src/communityuni/com/HocScanner.java
@@ -0,0 +1,15 @@
+package communityuni.com;
+
+import java.util.Scanner;
+
+public class HocScanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("nhâp số nguyên");
+		int a = sc.nextInt();
+
+	}
+
+}
