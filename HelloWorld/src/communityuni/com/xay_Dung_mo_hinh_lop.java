50332ece9e4317b9a561088d144a9b5a50a71b9f
dkiet519-spec <dkiet519@gmail.com>
2026-05-09 11:31:52 +0700

    sua xay dung mo hinh lop

--- HelloWorld/src/communityuni/com/xay_Dung_mo_hinh_lop.java
+++ HelloWorld/src/communityuni/com/xay_Dung_mo_hinh_lop.java
@@ -1,22 +1,43 @@
 package communityuni.com;
-
+import java.util.ArrayList;
 public class xay_Dung_mo_hinh_lop {
+	private ArrayList<StaffMember> staffList;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
 
 	}
+	//StaffMember
 	public abstract class StaffMember {
 	    protected String name;
 	    protected String address;
 	    protected String phone;
 
-	    public StaffMember(String name, String address, String phone) {
-	        this.name = name;
-	        this.address = address;
-	        this.phone = phone;
-	    }
+	    
+	    
 	}
+	//Volunteer
+	public class Volunteer extends StaffMember
+	{
+		
+	}
+	// Employee
+	public class Employee extends StaffMember {
+	    protected String socialSecurityNumber;
+	    protected double payRate;
+	}
+	//Executive
+	public class Executive extends Employee {
+	    private double bonus;
+	}
+	//Hourly
+	public class Hourly extends Employee {
+	    private int hoursWorked;
+	}
+	
+
+	
 
 }
