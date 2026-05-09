a44cc884ae8535d1a9a4b08331b4286eff75784d
dkiet519-spec <dkiet519@gmail.com>
2026-05-09 11:14:01 +0700

    xay dung mo hinh lop

---
+++ HelloWorld/src/communityuni/com/xay_Dung_mo_hinh_lop.java
@@ -0,0 +1,22 @@
+package communityuni.com;
+
+public class xay_Dung_mo_hinh_lop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+	public abstract class StaffMember {
+	    protected String name;
+	    protected String address;
+	    protected String phone;
+
+	    public StaffMember(String name, String address, String phone) {
+	        this.name = name;
+	        this.address = address;
+	        this.phone = phone;
+	    }
+	}
+
+}
